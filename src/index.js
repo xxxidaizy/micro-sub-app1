10bb3a592c4cf49da440e3e2ecb378918605c197
zhangqi <zhangqi@neolix.cn>
2024-08-27 20:28:58 +0800

    stash

--- src/index.js
+++ src/index.js
@@ -3,13 +3,38 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './public-path';
+
+let root = null;
+function render(props) {
+  const { container } = props;
+  root = root || ReactDOM.createRoot(container ? container.querySelector('#appRoot') : document.getElementById('appRoot'))
+  console.log(111111, window.__POWERED_BY_QIANKUN__);
+  root.render(
+    <BrowserRouter basename={window.__POWERED_BY_QIANKUN__ ? '/react-micro1' : '/'}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  );
+}
+
+if (!window.__POWERED_BY_QIANKUN__) {
+  render({});
+}
+export async function bootstrap() {
+  console.log("[react18] react app bootstraped");
+}
+export async function mount(props) {
+  console.log("[react18]props from main framework", props);
+  render(props);
+}
+export async function unmount(props) {
+  root.unmount();
+  root = null;
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
